2d6c15e037f8c584031f4e3a7009d651f904ce06
“Richard <richgurney1987@gmail.com>
2017-04-02 00:26:56 +0100

    intial commit

---
+++ config/routes.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var router = express.Router();
+
+var carController = require('../controllers/carController');
+var apiController = require('../controllers/apiController');
+
+// WEB
+router.route('/cars')
+  .get(carController.getAll)
+  .post(carController.createCarEntry)
+
+router.route('/cars/new')
+  .get(carController.newCar)
+
+router.route('/cars/:id/edit')
+  .get(carController.editCar)
+
+router.route('/cars/:id')
+  .get(carController.getCar)
+  .put(carController.updateCar)
+  .delete(carController.removeCar)
+
+// API
+router.route('/api/cars')
+  .get(apiController.getAll)
+  .post(apiController.createCarEntry)
+
+router.route('/api/cars/edit')
+  .put(apiController.updateCar)
+  .delete(apiController.removeCar)
+
+module.exports = router;
